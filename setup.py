3dbb3191f1f2633d3b22698c71540dfcc725db73
MaxPandy <142334021+MaxPandy@users.noreply.github.com>
2023-08-15 19:00:49 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='Guitar_Chord_Generator',
+      version='1.0',
+      description='Python Guitar Chord Generator',
+      author='Maximilián Pándy',
+      author_email='maxpandy123@gmail.com',
+      packages=['chords'],
+     )
