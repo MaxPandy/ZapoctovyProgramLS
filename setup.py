867404847ad3d1b7303dba60d2c6cde785ffc6a6
MaxPandy <142334021+MaxPandy@users.noreply.github.com>
2023-08-15 19:27:43 +0200

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup
-
-setup(name='Guitar_Chord_Generator',
-      version='1.0',
-      description='Python Guitar Chord Generator',
-      author='Maximilián Pándy',
-      author_email='maxpandy123@gmail.com',
-      packages=['chords'],
-     )
